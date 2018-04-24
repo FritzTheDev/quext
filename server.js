0478533e119f81f2c2454753d8037fabf15d713d
ottovbismarck <fritzj96@gmail.com>
2018-04-23 18:01:25 -0700

    Progress 6PM 423

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
+//module reqs
+require('dotenv').load();
 const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+// get express ready
 const app = express();
+// get mongoose set up
+mongoose.connect('mongodb://'+ process.env.UN +":" + process.env.PW + '@ds157089.mlab.com:57089/quext');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.post('/', (req, res) => {
   const twiml = new MessagingResponse();
-  console.log(req.body.Body)
+  console.log(req.body.Body);
   twiml.message('This app works!');
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
