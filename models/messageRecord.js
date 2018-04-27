a5b802d4ecbb95d6e2410561d5f7696dc2a1f53b
ottovbismarck <fritzj96@gmail.com>
2018-04-26 22:07:48 -0700

    got database working

--- models/messageRecord.js
+++ models/messageRecord.js
@@ -1,11 +1,20 @@
-const mongoose = require('mongoose');
+//FOR REFERENCE ONLY:
+// const mongoose = require('mongoose');
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const MessageRecordSchema = new Schema({
-    Username: String,
-    TimeStamp: Date,
-    Body: String
-});
+// const MessageRecordSchema = new Schema({
+//     Phone: String,
+//     TimeStamp: Date,
+//     Body: String
+// });
 
-const MessageRecordModel =  mongoose.model('MessageRecordModel', MessageRecordSchema);
+// const MessageRecordModel = mongoose.model('MessageRecordModel', MessageRecordSchema);
+
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/test');
+
+// const Cat = mongoose.model('Cat', { name: String });
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-//module reqs
+// Module reqs
 require('dotenv').load();
 const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// get express ready
+// Get express ready
 const app = express();
+// Add Schema
+const Schema = mongoose.Schema;
+
+const MessageRecordSchema = mongoose.model('Message', {
+    From: String,
+    TimeStamp: Date,
+    Body: String
+});
 // get mongoose set up
 mongoose.connect('mongodb://'+ process.env.UN +":" + process.env.PW + '@ds157089.mlab.com:57089/quext');
 mongoose.Promise = global.Promise;
@@ -20,7 +28,8 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.post('/', (req, res) => {
   const twiml = new MessagingResponse();
-  console.log(req.body.Body);
+  const message = new MessageRecordSchema({ From: req.body.From, Body: req.body.Body})
+  message.save().then(() => console.log(req.body.Body))
   twiml.message('This app works!');
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
